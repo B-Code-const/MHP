921d1af9aabc6dc227c4397c10e6e2cb4e1a43ea
B-Code-const <242734118+B-Code-const@users.noreply.github.com>
2026-07-09 19:23:43 -0400

    added styling and journal textarea

--- journal.php
+++ journal.php
@@ -7,6 +7,12 @@ include 'includes/header.php';
     <h1>Mood Journal</h1>
     <p>This is the main content area of the journal page.</p>
 </main>
+<label for="w3review">Review of W3Schools:</label>
+
+<textarea id="w3review" name="w3review" rows="10" cols="50">
+At w3schools.com you will learn how to make a website. They offer free tutorials in all web development technologies.
+</textarea>
+
 
 <?php
 include 'includes/footer.php';
